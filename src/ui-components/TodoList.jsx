9b8ac2eacbeea2eb5264edfe1c837f868477dd1a
Alistair Colling <a@colli.ng>
2023-07-15 17:17:32 +0100

    todolist showing!

--- src/ui-components/TodoList.jsx
+++ src/ui-components/TodoList.jsx
@@ -1,31 +1,44 @@
 //a component to list all Todos from aws amplify DataStore
 import React from "react";
-import { Todo } from "../models";
-import { DataStore } from "aws-amplify";
 import { Grid } from "@chakra-ui/react";
 import { API } from '@aws-amplify/api'
-import config from './aws-exports'
-import { listTodos } from './graphql/queries'
+// import config from './aws-exports'
+import config from '../aws-exports'
+import { listTodos } from '../graphql/queries'
+import { DataStore } from '@aws-amplify/datastore';
+import { Todo } from '../models';
+import { set } from "lodash";
 
 // after your imports
 API.configure(config)
 
 async function list() {
+	console.log("list............");
    const response = await API.graphql({
       query: listTodos,
       variables: {
       // <your variables, optional>
       },
    })
-	  console.log("response");
+	  console.log("response.....");
    console.log(response);
 }
+
+
 
 export default function TodoList(props) {
   const { todos, onTodoClick, overrides, ...rest } = props;
   const [todoRecords, setTodoRecords] = React.useState(todos);
+	const dataStoreQuery = async () => {
+		const todos = await DataStore.query(Todo);
+		console.log("datastore todos...............");
+		console.log(todos);
+		setTodoRecords(todos);
+	}
   React.useEffect(() => {
-		list();
+		// list();
+		const todos = dataStoreQuery()
+
   }, []);
   return (
     <Grid
@@ -36,7 +49,7 @@ export default function TodoList(props) {
       {...overrides}
       {...rest}
     >
-      {/* {todoRecords.map((todo) => (
+      {todoRecords?.length && todoRecords.map((todo) => (
         <Grid
           as="li"
           key={todo.id}
@@ -45,7 +58,7 @@ export default function TodoList(props) {
         >
           {todo.name}
         </Grid>
-      ))} */}
+      ))}
     </Grid>
   );
 }
