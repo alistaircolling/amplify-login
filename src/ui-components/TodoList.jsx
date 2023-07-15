bedbb78ac39848f5a7b988481a7b96aaaf8d790c
Alistair Colling <a@colli.ng>
2023-07-15 17:02:20 +0100

    using graph to get todos

--- src/ui-components/TodoList.jsx
+++ src/ui-components/TodoList.jsx
@@ -3,18 +3,29 @@ import React from "react";
 import { Todo } from "../models";
 import { DataStore } from "aws-amplify";
 import { Grid } from "@chakra-ui/react";
+import { API } from '@aws-amplify/api'
+import config from './aws-exports'
+import { listTodos } from './graphql/queries'
+
+// after your imports
+API.configure(config)
+
+async function list() {
+   const response = await API.graphql({
+      query: listTodos,
+      variables: {
+      // <your variables, optional>
+      },
+   })
+	  console.log("response");
+   console.log(response);
+}
 
 export default function TodoList(props) {
   const { todos, onTodoClick, overrides, ...rest } = props;
   const [todoRecords, setTodoRecords] = React.useState(todos);
   React.useEffect(() => {
-    const queryData = async () => {
-      const records = await DataStore.query(Todo);
-			console.log('records');
-			console.log(records);
-      setTodoRecords(records);
-    };
-    queryData();
+		list();
   }, []);
   return (
     <Grid
