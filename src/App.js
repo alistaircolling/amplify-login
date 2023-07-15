4a474780bd8c144c51f585c03407f46adf7bbe30
Alistair Colling <a@colli.ng>
2023-07-15 14:43:50 +0100

    added user email hello

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import { Button, Heading, withAuthenticator } from '@aws-amplify/ui-react';
 
 /* src/App.js */
 function App({ signOut, user }) {
+  console.log(user);
   return (
     <div className="App">
-      <Heading level={1}>Hello {user.username}</Heading>
+      <Heading level={1}>Hello {user.attributes.email}</Heading>
       <Button onClick={signOut}>Sign out</Button>
     </div>
   );
